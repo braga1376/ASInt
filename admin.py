8146c2a22ef67ff378bb970df66d6c7731fb7147
braga1376 <braga1376@gmail.com>
2018-12-26 16:06:12 +0000

    asint

---
+++ admin.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import copy
+import sys
+
+
+class Admin:
+
+    def __init__(self, host):
+        self.host = host
+
+    def getFromURI(self, uri):# receive values from request
+        r = requests.get(self.host + uri)
+        print(r)
+        data = r.json()
+        print(data)
+
+    def postToURI(self, uri, d):
+        r = requests.post(self.host + uri, data = d )
+        print(r.text)
+
+    def readFileToJSon(self, f):
+        d = {}
+        dic = []
+        for line in f:
+            s = line.replace('\n','').split(' ')
+            d['id'] = s[0]
+            d['name'] = s[1].replace('_', ' ')
+            d['x'] = s[2]
+            d['y'] = s[3]
+            dic.append((copy.deepcopy(d)))
+        return json.dumps(dic)
+            
+
+    def menu(self):#
+
+        #g = geocoder.ip('me')
+        #print(g.latlng)
+        while (1):
+            print(
+                '\nCommands available:\n\t1 - DEFINE BUILDINGS '
+                '\n\t2 - LIST USERS  \n\t3 - LIST USERS IN A BUILDING '
+                '\n\t4 - USER LOGS\n\t5 - BUILDING LOGS\n\t6 - EXIT')
+            command = input("\n> ")
+            if (command == "1"):
+                file = open('buildings',"r", encoding = "utf-8-sig")
+                j = self.readFileToJSon(file)
+                print(j)
+                uri = '/API/Admin/Buildings'
+                self.postToURI(uri, j)
+
+            elif (command == "2"):
+                uri = '/API/Admin/Users'
+                self.getFromURI(uri)
+
+            elif (command == "3"):
+                buildingid = input("\n\tBuilding Identifier:\n\t\t")
+                uri = '/API/Admin/Buildings/' + buildingid + '/Users'
+                self.getFromURI(uri)
+
+            elif (command == "4"):
+                userid = input("\n\tUser Identifier:\n\t\t")
+                uri = '/API/Admin/Logs/Users/' + userid
+                self.getFromURI(uri)
+
+            elif (command == "5"):# é preciso? não vejo no enunciado
+                buildingid = input("\nBuilding Identifier:\n\t\t")
+                uri = '/API/Admin/Logs/Buildings/' + buildingid
+                self.getFromURI(uri)
+
+            elif (command == "6"):
+                return
+
+            else:
+                print("'%s' is not a valid command" % (command))
+
+def main():
+    admin = Admin('http://127.0.0.1:5000')
+    admin.menu()
+    # admin = Admin(argv)2
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ user.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class User:
+
+	def __init__(self, id):
+        self.host = id
+
+
+
+
+def main():
+    user = User(2)
+
+    
+    # admin = Admin(argv)2
+
+
+if __name__ == "__main__":
+    main()

c6fa49b9cefaa325ecac817369a987cdfc51d701
braga1376 <braga1376@gmail.com>
2018-12-27 01:37:37 +0000

    server and admin running

--- server/user.py
+++ server/user.py
@@ -6,15 +6,10 @@ class User:
 	def __init__(self, id):
         self.host = id
 
-
-
-
 def main():
     user = User(2)
-
     
     # admin = Admin(argv)2
-
 
 if __name__ == "__main__":
     main()
